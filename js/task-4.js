54c57c01ae93d0ac3d7504bd4d3b4d698c6b3ad7
Oleksii0112 <paganizondatr75@gmail.com>
2026-04-05 14:23:29 +0300

    tasks 1-4

---
+++ js/task-4.js
@@ -0,0 +1,16 @@
+function getShippingCost(country) {
+    let list = { "China": 100, "Chile": 250, "Australia": 170, "Jamaica": 120 }
+    let price = list[country];
+    if (price) {
+        return `Shipping to ${country} will cost ${price} credits`;
+    }
+    else {return "Sorry, there is no delivery to your country"
+    }
+}
+
+console.log(getShippingCost("Australia")); // "Shipping to Australia will cost 170 credits"
+console.log(getShippingCost("Germany")); // "Sorry, there is no delivery to your country"
+console.log(getShippingCost("China")); // "Shipping to China will cost 100 credits"
+console.log(getShippingCost("Chile")); // "Shipping to Chile will cost 250 credits"
+console.log(getShippingCost("Jamaica")); // "Shipping to Jamaica will cost 120 credits"
+console.log(getShippingCost("Sweden")); // "Sorry, there is no delivery to your country"
